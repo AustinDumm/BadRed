1942c66cb21bf11c1f34b844c7ece8666de1a911
Austin Dumm <austin.dumm@gmail.com>
2024-05-18 16:47:59 -0400

    Add support for terminal alternate and raw mode

--- bad_red/src/main.rs
+++ bad_red/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Stdout, Write};
+
+use crossterm::{event::{read, Event, KeyCode}, queue, terminal::*};
+
+const TITLE: &str = "BadREd";
+
+fn main() -> io::Result<()> {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    if let Err(err) = setup_terminal(&mut stdout) {
+        let _ = cleanup_terminal(&mut stdout);
+        return Err(err)
+    }
+
+    loop {
+        match read()? {
+            Event::Key(event) if event.code == KeyCode::Esc => break,
+            event => println!("EVENT: {:?}", event),
+        }
+    }
+
+    cleanup_terminal(&mut stdout)
+}
+
+fn setup_terminal(stdout: &mut Stdout) -> io::Result<()> {
+    queue!(stdout, EnterAlternateScreen, SetTitle(TITLE))?;
+    stdout.flush()?;
+
+    enable_raw_mode()
+}
+
+fn cleanup_terminal(stdout: &mut Stdout) -> io::Result<()> {
+    queue!(stdout, LeaveAlternateScreen)?;
+    stdout.flush()?;
+
+    disable_raw_mode()
 }
