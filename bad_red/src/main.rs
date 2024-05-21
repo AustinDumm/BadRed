24bdae0d7456194b04d0543e08744f61ad1a4081
Austin Dumm <austin.dumm@gmail.com>
2024-05-21 16:19:59 -0400

    Cleanup render into terminal

--- bad_red/src/main.rs
+++ bad_red/src/main.rs
@@ -56,6 +56,87 @@ impl Terminal {
 
         disable_raw_mode()
     }
+
+    fn render(&mut self, editor_state: &EditorState) -> io::Result<()> {
+        let window_size = terminal::window_size()?;
+        let editor_size = EditorSize {
+            x_row: 0,
+            y_col: 0,
+            rows: window_size.rows,
+            cols: window_size.columns,
+        };
+
+        for (buffer_index, buffer_pane_path) in editor_state.buffer_panes.iter().enumerate() {
+            let Some(buffer_pane_path) = buffer_pane_path else {
+                continue;
+            };
+
+            let buffer = &editor_state.buffers[buffer_index];
+            let Some((pane, size)) = editor_state
+                .root_pane
+                .leaf_and_size_at_path(&editor_size, *buffer_pane_path)
+            else {
+                writeln!(io::stderr(), "Failed attempt to find pane by path")?;
+                continue;
+            };
+
+            queue!(self.stdout, cursor::MoveTo(size.y_col, size.x_row))?;
+            let mut buffer_row = 0;
+            let mut buffer_col = 0;
+            let mut cursor_position: Option<(u16, u16)> = None;
+            let mut chars = buffer.content.chars().peekable();
+            let mut char_count = 0;
+
+            while let Some(char) = chars.peek() {
+                if buffer_row == pane.top_line {
+                    break;
+                }
+
+                if *char == '\n' || *char == '\r' {
+                    let char = chars.next().unwrap();
+                    if handle_newline(char, &mut char_count, &mut chars) {
+                        buffer_row += 1;
+                    }
+                }
+            }
+
+            while let Some(char) = chars.next() {
+                char_count += 1;
+                let is_newline = handle_newline(char, &mut char_count, &mut chars);
+                if is_newline {
+                    queue!(self.stdout, style::Print(" "))?;
+                    for _ in buffer_col..size.cols - 2 {
+                        queue!(self.stdout, Clear(ClearType::UntilNewLine))?;
+                    }
+                    queue!(self.stdout, style::Print("\n\r"))?;
+                    buffer_row += 1;
+                    buffer_col = 0;
+                } else {
+                    queue!(self.stdout, style::Print(char))?;
+                    buffer_col += 1;
+                }
+
+                if char_count == buffer.cursor_index {
+                    cursor_position = Some((buffer_row, buffer_col));
+                }
+            }
+
+            for _ in buffer_row..size.rows {
+                for _ in buffer_col..size.cols {
+                    queue!(self.stdout, style::Print(" "))?;
+                }
+                buffer_col = 0;
+            }
+
+            if let Some(cursor_position) = cursor_position {
+                queue!(self.stdout, cursor::MoveTo(cursor_position.1, cursor_position.0))?;
+            } else {
+                panic!()
+            }
+        }
+
+        self.stdout.flush()
+    }
 }
 
 impl Drop for Terminal {
@@ -87,7 +168,7 @@ fn main() -> io::Result<()> {
             }
         }
 
-        render(&mut terminal, &editor_state)?;
+        terminal.render(&editor_state)?;
     }
 
     Ok(())
@@ -151,87 +232,6 @@ impl EditorSize {
     }
 }
 
-fn render(terminal: &mut Terminal, editor_state: &EditorState) -> io::Result<()> {
-    let stdout = &mut terminal.stdout;
-    let window_size = terminal::window_size()?;
-    let editor_size = EditorSize {
-        x_row: 0,
-        y_col: 0,
-        rows: window_size.rows,
-        cols: window_size.columns,
-    };
-
-    for (buffer_index, buffer_pane_path) in editor_state.buffer_panes.iter().enumerate() {
-        let Some(buffer_pane_path) = buffer_pane_path else {
-            continue;
-        };
-
-        let buffer = &editor_state.buffers[buffer_index];
-        let Some((pane, size)) = editor_state
-            .root_pane
-            .leaf_and_size_at_path(&editor_size, *buffer_pane_path)
-        else {
-            writeln!(io::stderr(), "Failed attempt to find pane by path")?;
-            continue;
-        };
-
-        queue!(stdout, cursor::MoveTo(size.y_col, size.x_row))?;
-        let mut buffer_row = 0;
-        let mut buffer_col = 0;
-        let mut cursor_position: Option<(u16, u16)> = None;
-        let mut chars = buffer.content.chars().peekable();
-        let mut char_count = 0;
-
-        while let Some(char) = chars.peek() {
-            if buffer_row == pane.top_line {
-                break;
-            }
-
-            if *char == '\n' || *char == '\r' {
-                let char = chars.next().unwrap();
-                if handle_newline(char, &mut char_count, &mut chars) {
-                    buffer_row += 1;
-                }
-            }
-        }
-
-        while let Some(char) = chars.next() {
-            char_count += 1;
-            let is_newline = handle_newline(char, &mut char_count, &mut chars);
-            if is_newline {
-                queue!(stdout, style::Print(" "))?;
-                for _ in buffer_col..size.cols - 2 {
-                    queue!(stdout, Clear(ClearType::UntilNewLine))?;
-                }
-                queue!(stdout, style::Print("\n\r"))?;
-                buffer_row += 1;
-                buffer_col = 0;
-            } else {
-                queue!(stdout, style::Print(char))?;
-                buffer_col += 1;
-            }
-
-            if char_count == buffer.cursor_index {
-                cursor_position = Some((buffer_row, buffer_col));
-            }
-        }
-
-        for _ in buffer_row..size.rows {
-            for _ in buffer_col..size.cols {
-                queue!(stdout, style::Print(" "))?;
-            }
-            buffer_col = 0;
-        }
-
-        if let Some(cursor_position) = cursor_position {
-            queue!(stdout, cursor::MoveTo(cursor_position.1, cursor_position.0))?;
-        } else {
-            panic!()
-        }
-    }
-
-    stdout.flush()
-}
 
 fn handle_newline<I>(char: char, char_count: &mut usize, chars: &mut Peekable<I>) -> bool
 where
